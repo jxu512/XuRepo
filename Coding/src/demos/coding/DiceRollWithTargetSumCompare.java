74a8ed6701fe5eda7bd8ddcf780373b91fe40acb
Jeff Xu <jxu512@gmail.com>
2022-02-25 20:59:47 -0500

    word ladder

--- Coding/src/demos/coding/DiceRollWithTargetSumCompare.java
+++ Coding/src/demos/coding/DiceRollWithTargetSumCompare.java
@@ -17,16 +17,22 @@ public class DiceRollWithTargetSumCompare {
     	int target=Integer.parseInt(args[2]);
     	
     	System.out.format("Input: %d, %d, %d\n", n,k,target);
+    	DiceRollWithTargetSumBacktracking roll4 = new DiceRollWithTargetSumBacktracking();
     	DiceRollWithTargetSumRecursion roll1 = new DiceRollWithTargetSumRecursion();
     	DiceRollWithTargetSumTopDown roll2 = new DiceRollWithTargetSumTopDown();
     	DiceRollWithTargetSumBottomUp roll3 = new DiceRollWithTargetSumBottomUp();
-    	DiceRollWithTargetSumBacktracking roll4 = new DiceRollWithTargetSumBacktracking();
     	
     	long t1,t2;
     	int num=0;
     	String loops=null;
     	t1 = System.currentTimeMillis();
-    	num = roll1.numRollsToTarget(n, k, target);
+    	//num = roll4.numRollsToTarget(n, k, target);
+    	t2 = System.currentTimeMillis();
+    	loops = NumberFormat.getNumberInstance(Locale.US).format(roll4.loops);
+    	System.out.format("Number: %d, Time: %d ms, loops: %s\n",num, (t2-t1)/2, loops);
+    	
+    	t1 = System.currentTimeMillis();
+    	//num = roll1.numRollsToTarget(n, k, target);
     	t2 = System.currentTimeMillis();
     	loops = NumberFormat.getNumberInstance(Locale.US).format(roll1.loops);
     	System.out.format("Number: %d, Time: %d ms, loops: %s\n",num, (t2-t1)/2, loops);
@@ -43,11 +49,6 @@ public class DiceRollWithTargetSumCompare {
     	loops = NumberFormat.getNumberInstance(Locale.US).format(roll3.loops);
     	System.out.format("Number: %d, Time: %d ms, loops: %s\n",num, (t2-t1)/2, loops);
 
-    	t1 = System.currentTimeMillis();
-    	num = roll4.numRollsToTarget(n, k, target);
-    	t2 = System.currentTimeMillis();
-    	loops = NumberFormat.getNumberInstance(Locale.US).format(roll4.loops);
-    	System.out.format("Number: %d, Time: %d ms, loops: %s\n",num, (t2-t1)/2, loops);
 }
 
 
---
+++ Coding/src/demos/coding/WordLadder.java
@@ -0,0 +1,75 @@
+package demos.coding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+      
+        Queue<String> queue = new LinkedList<String>();
+        
+        int depth = findWord(beginWord, endWord,wordList, queue);
+        System.out.println("Length: "+depth);
+        return depth;
+    }
+    
+    private int findWord(String currentWord, String endWord,List<String> wordList,Queue<String> queue) {
+        
+        queue.add(currentWord);
+        int depthCount=queue.size();
+        int depth=1;
+        while(!queue.isEmpty()) {
+            
+            String word = queue.remove();
+            List<String> match = matchWords(word,wordList);
+            for(int i=0;i<match.size();i++) {
+                String next = match.get(i);
+                if(next.equals(endWord)) return depth+1; // Move up one level
+                queue.add(next);
+            }
+            if(--depthCount==0) { 
+            	depth+=1; 
+            	depthCount=queue.size();
+            }
+        }
+        return 0;
+    }
+    private List<String> matchWords(String word, List<String> wordList){
+        
+        List<String> match = new ArrayList<String>();
+        for(int i=0;i<wordList.size();i++) {
+            int count=0;
+            for(int j=0;j<word.length();j++)
+                	if(word.charAt(j)!=wordList.get(i).charAt(j)) count ++;
+            if(count==1)  match.add(wordList.get(i));
+        }
+        wordList.removeAll(match);
+        return match;
+    }
+    
+    public static void main(String[] args) {
+    	
+    	WordLadder ladder = new WordLadder();
+    	/*
+    	String begin="hit";
+    	String end="cog";
+    	String[] arr = {"hot","dot","dog","lot","log","cog"};
+    	*/
+    	/*
+    	String begin="a";
+    	String end="c";
+    	String[] arr = {"a","b","c"};
+    	*/
+    	//*
+    	String begin="leet";
+    	String end="code";
+    	String[] arr = {"leet","code","lest","leet","lose","code","lode","robe","lost"};
+    	//*/
+    	List<String> list = new ArrayList<String>();
+    	list.addAll(Arrays.asList(arr));
+    	ladder.ladderLength(begin, end, list);
+    }
+}
