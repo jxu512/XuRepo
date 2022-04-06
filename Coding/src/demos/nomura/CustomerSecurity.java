4ae1e8d19fd865cffe8fc74a3581a30bd46d3c41
Jeff Xu <jxu512@gmail.com>
2022-04-05 21:36:32 -0400

    update

--- Coding/src/demos/nomura/CustomerSecurity.java
+++ Coding/src/demos/nomura/CustomerSecurity.java
@@ -11,13 +11,31 @@ Calculate net qty
 */
 package demos.nomura;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CustomerSecurity {
 
-	public Map<CustSecurity, Integer> calcQut(List<Order> orders) {
+	public static void main(String[] args) {
+		List<Order> orders=new ArrayList<>();
+		orders.add(new Order("ABC","Telsa",200));
+		orders.add(new Order("ABC","Telsa",-100));
+		orders.add(new Order("ABC","Apple",200));
+		orders.add(new Order("ABC","Apple",100));
+		orders.add(new Order("XYZ","Telsa",200));
+		orders.add(new Order("XYZ","Telsa",-100));
+		orders.add(new Order("XYZ","Apple",200));
+		orders.add(new Order("XYZ","Apple",100));
+		
+		CustomerSecurity cs = new CustomerSecurity();
+		System.out.println("For loop:"+cs.calcQty(orders));
+		System.out.println("Stream:  "+cs.calcQtyStream(orders));
+	}
+	// Traditional
+	public Map<CustSecurity, Integer> calcQty(List<Order> orders) {
 		Map<CustSecurity, Integer> map = new HashMap<>();
 		
 		for(Order o: orders) {
@@ -27,12 +45,24 @@ public class CustomerSecurity {
 		}
 		return map;
 	}
+	// Stream
+	public Map<CustSecurity, Integer> calcQtyStream(List<Order> orders) {
+		Map<CustSecurity, Integer> map = 
+			orders.stream().collect(Collectors.groupingBy(o->new CustSecurity(o.cust,o.security), Collectors.summingInt(o->o.qty)));
+		
+		return map;
+	}
 }
 
 class Order{
 	String cust;
 	String security;
 	int qty;
+	public Order(String c,String s,int q) {
+		cust=c;
+		security=s;
+		qty=q;
+	}
 }
 class CustSecurity{
 	String cust;
@@ -42,12 +72,15 @@ class CustSecurity{
 		cust=c;
 		security=s;
 	}
+	public String toString() {
+		return cust+"-"+security;
+	}
 	// To use CustSecurity as hask key, must override equals and hashCode
 	@Override
 	public boolean equals(Object o) {
 		if(o==null || ! (o instanceof CustSecurity)) return false;
 		CustSecurity co = (CustSecurity)o;
-		return cust.equals(co) && security.equals(co.security);
+		return cust.equals(co.cust) && security.equals(co.security);
 	}
 	@Override
 	public int hashCode() {
