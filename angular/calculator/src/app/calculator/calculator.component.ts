cf5eaa5d46a775ede7d4da690ba569a050bfbdee
Jeff Xu <jxu512@gmail.com>
2022-05-14 18:21:07 -0400

    Update calculator.component.ts

--- angular/calculator/src/app/calculator/calculator.component.ts
+++ angular/calculator/src/app/calculator/calculator.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule,ReactiveFormsModule   } from '@angular/forms';
-imports: [
-         FormsModule, ReactiveFormsModule
-      ]
       
 @Component({
   selector: 'app-calculator',
