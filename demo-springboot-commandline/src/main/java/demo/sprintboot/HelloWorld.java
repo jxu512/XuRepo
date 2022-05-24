6f6e980ac73dd869572a7f0b9f560e9bcaf30139
Jeff Xu <jxu512@gmail.com>
2022-05-23 20:40:54 -0400

    spring boot web

--- demo-springboot-commandline/src/main/java/demo/sprintboot/HelloWorld.java
+++ demo-springboot-commandline/src/main/java/demo/sprintboot/HelloWorld.java
@@ -1,9 +1,10 @@
 package demo.sprintboot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+// @Component or @Service when no Configuration defined
 public class HelloWorld {
 	@Value("${name}")
 	private String name;
--- demo-springboot-commandline/src/main/java/demo/sprintboot/SpringBootConsoleApplication.java
+++ demo-springboot-commandline/src/main/java/demo/sprintboot/SpringBootConsoleApplication.java
@@ -1,16 +1,16 @@
 package demo.sprintboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class SpringBootConsoleApplication implements CommandLineRunner {
 
 	@Autowired
 	private HelloWorld hello;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         SpringApplication.run(SpringBootConsoleApplication.class, args);
 		
