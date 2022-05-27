7bf9ad9c40799da0b1cadbf8c77b2a68e5aa63cd
Jeff Xu <jxu512@gmail.com>
2022-05-27 17:52:15 -0400

    deploy war to standalone Tomcat

--- demo-springboot-web/src/main/java/demo/sprintbootweb/HelloApplication.java
+++ demo-springboot-web/src/main/java/demo/sprintbootweb/HelloApplication.java
@@ -2,14 +2,20 @@ package demo.sprintbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloApplication {
-
+public class HelloApplication extends SpringBootServletInitializer {
+	
     public static void main(String[] args) {
 
         SpringApplication.run(HelloApplication.class, args);
 		
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloApplication.class);
     }
 
 }
--- demo-springboot-web/src/main/java/demo/sprintbootweb/HelloWorld.java
+++ demo-springboot-web/src/main/java/demo/sprintbootweb/HelloWorld.java
@@ -1,5 +1,7 @@
 package demo.sprintbootweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam String name) {
-		return "Hello "+name+"!";	}
+		String msg = "Hello "+name+"!";
+		log.info(name);
+		return msg;	
+	}
 }
