ae18c3ee6237335817e37ab006470f729b34907c
Jeff Xu <73799409+jxu512@users.noreply.github.com>
2024-05-01 11:19:05 -0400

    SQL
    SQL

---
+++ SQL/Pivot.sql
@@ -0,0 +1,28 @@
+--with Compony as
+create table Company(id, name, country) as
+(
+    select 1 as id, 'IBM' as name, 'USA' as Country from dual
+    union
+    select 2 as id, 'HP' as name, 'USA' as Country from dual
+    union
+    select 3 as id, 'Barclays' as name, 'UK' as Country from dual
+    union
+    select 4 as id, 'Simens' as name, 'Germany' as Country from dual
+    union
+    select 5 as id, 'Alibaba' as name, 'China' as Country from dual
+    union
+    select 6 as id, 'Volvo' as name, 'Sweden' as Country from dual
+    union
+    select 7 as id, 'Bank of China' as name, 'China' as Country from dual
+    union
+    select 8 as id, 'Toyota' as name, 'Japan' as Country from dual
+    union
+    select 9 as id, 'Honda' as name, 'Japan' as Country from dual
+)
+-- ";" needed for create table but not for with clause
+;
+select * from
+( select name, country from Company )
+pivot ( count(name) for Country in ( 'USA', 'UK', 'Germany', 'Sweden', 'China', 'Japan' ) )
+--pivot XML ( count(name) for Country in ( select Country from Company ) )
+;
